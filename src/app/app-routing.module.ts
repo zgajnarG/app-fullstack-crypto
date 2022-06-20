e5034fcd2bf8f325f9a2b51e242b2eeab4fb1370
Alec Narayanin <narayanin.alec@hotmail.com>
2022-06-20 09:22:52 +0200

    WIP : begin crypto-details cmp

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'test',
+    loadChildren : () => import('./test/test.module').then(m => m.TestModule)
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WebfullstackDesignSystemModule } from 'webfullstack-design-system';
 import { CryptoCardComponent } from './crypto-card/crypto-card.component';
+import { CryptoDetailComponent } from './crypto-detail/crypto-detail.component';
 
 const COMPONENTS = [CryptoCardComponent];
 
 @NgModule({
-  declarations: [...COMPONENTS, AppComponent],
+  declarations: [...COMPONENTS, AppComponent, CryptoDetailComponent],
   imports: [BrowserModule, AppRoutingModule, WebfullstackDesignSystemModule],
   exports: [...COMPONENTS],
   providers: [],
