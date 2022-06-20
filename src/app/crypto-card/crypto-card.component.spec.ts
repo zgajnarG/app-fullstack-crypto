3c712e07a7a8e71df8c24ae8ce5cf13b99eafc87
dryisf <daryl.issoufi@gmail.com>
2022-06-20 09:15:08 +0200

    feat: crypto card

---
+++ src/app/crypto-card/crypto-card.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CryptoCardComponent } from './crypto-card.component';
+
+describe('CryptoCardComponent', () => {
+  let component: CryptoCardComponent;
+  let fixture: ComponentFixture<CryptoCardComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CryptoCardComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CryptoCardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
